3018aa5a04090e2d4f17f01e902d51d35fc4fc43
Cristian-263 <csobrinom@alumnos.nebrija.es>
2025-11-24 10:48:31 +0100

    He configurado las rutas y completado el componente login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent, 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+
+    ]
+  }
+];
