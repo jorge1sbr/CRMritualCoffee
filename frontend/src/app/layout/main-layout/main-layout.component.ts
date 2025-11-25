186034401b1dd3bbe01908f75c9ed99ef9533578
Adrian8 <acatalans@alumnos.nebrija.es>
2025-11-25 13:05:19 +0100

    Activo botón de logout

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 // Módulos de Material para el diseño
@@ -8,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -27,5 +28,10 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent {
-  // Aquí puedes añadir lógica para el botón de "Cerrar Sesión" más adelante
+  
+  constructor(private router: Router, private authService: AuthService) {}
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
 }
